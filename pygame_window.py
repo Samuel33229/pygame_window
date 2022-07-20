bb25d4519785ebecfb23100705182feea3527ddd
samuel <samuelsontsf@gmail.com>
2022-07-20 15:57:19 -0400

    Añade un color a la ventana

--- pygame_window.py
+++ pygame_window.py
@@ -4,11 +4,22 @@ import pygame
 # Inicia los módulos de pygame
 pygame.init()
 
+# Colores de fondo RGB
+BLUE  = (0,0,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
 # Abre la ventana
-pygame.display.set_mode((400, 500))
+window = pygame.display.set_mode((400, 500))
 
 # Establece el título de la ventana
 pygame.display.set_caption("Mi primera ventana en PyGame")
+
+# Establece un color de fondo para la ventana
+window.fill(RED)
+pygame.display.flip()
 
 # Crea un valor booleano 
 run = True
